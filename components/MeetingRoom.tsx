7781ce04fa99cfa9dd6ebb39dfc1f1f0a1ee8473
yahyaracab <yahyaracab@gmail.com>
2024-05-29 21:50:41 +0000

    Mobile contrle bar fixed

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -52,7 +52,7 @@ const MeetingRoom = () => {
             </div>
         </div>
 
-        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 fixed-wrap">
+        <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
             <CallControls onLeave={() => {router.push('/')}}/>
             <DropdownMenu>
                 <div className="flex items-center">
